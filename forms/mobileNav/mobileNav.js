b6ab2071ead21a3519f3ae08893aeff17864d3ce
Maura Small <maurasmall@creighton.edu>
2021-10-27 15:04:31 -0500

    mobileNav

--- forms/mobileNav/mobileNav.js
+++ forms/mobileNav/mobileNav.js
@@ -1,7 +1,27 @@
 
+
 hmbMenu.onclick=function(s){
- // add code to check if user clicked the hamburger control to open it 
+  if (typeof(s) == "object"){
+    return
   /* add a switch here to take the user to the form
      they chose. 
   */
+  } else {
+      switch(s) {
+        case "Dessert Voting":
+          ChangeForm(dessertVoting)
+          break
+        case "Describe You":
+          ChangeForm(describeYou)
+          break
+        case "Fav Exercises":
+          ChangeForm(favExercises)
+          break
+        case "Mobile Nav":
+          ChangeForm(mobileNav)
+          break
+  }
 }
+}
+
+
